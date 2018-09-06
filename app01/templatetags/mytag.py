116c18379809eeb35abe27465b7d926c366910ed
shunchang.yuan <shunchang.yuan@24tidy.com>
2018-09-06 21:29:04 +0800

    simple_tag.

--- app01/templatetags/mytag.py
+++ app01/templatetags/mytag.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.simple_tag
 def get_book(num=3):
     return Book.objects.all()[:num]
+
+
+@register.simple_tag(name="addsum")
+def add_sum(*args):
+    ret = ""
+    for item in args:
+        ret = ret + item
+    return ret
